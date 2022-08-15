fc059b57565487ec138a75e3a0495806f3f41089
GSemix <63607553+GSemix@users.noreply.github.com>
2022-08-15 15:15:19 +0300

    Initial Commit

---
+++ NavigationMSU/NavigationMSUApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationMSUApp.swift
+//  NavigationMSU
+//
+//  Created by Семен Безгин on 15.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct NavigationMSUApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
